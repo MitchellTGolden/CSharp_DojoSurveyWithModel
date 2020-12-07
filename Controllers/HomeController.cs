edf7394c1b65742546408702bbc69f635e5646c5
Mitchell Golden <MitchellTGolden@gmail.com>
2020-12-07 13:14:32 -0600

    added assignment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("result")]
+        public IActionResult Result(string name, string dojo, string favLang, string comment)
+        {
+            Survey results = new Survey()
+            {
+
+                Name = name,
+                Dojo = dojo,
+                FavoriteLanguage = favLang,
+                Comment = comment
+            };
+            return View(results);
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Dojo { get; set; }
+        public string FavoriteLanguage { get; set; }
+        public string Comment { get; set; }
+    }
+}
